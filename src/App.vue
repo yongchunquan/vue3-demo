dcbd400470dc908a7ea9f8d680514bcd1515ce9f
Guxingzhe <Wiki20160620@163.com>
2021-02-19 09:35:00 +0800

    初始化工程vue3+vite+less/sass

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <div class="main">main</div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+    created: function () {
+        //   {"BASE_URL":"/","MODE":"development","DEV":true,"PROD":false}
+        console.info(JSON.stringify(import.meta.env.VITE_APP_TITLE));
+        console.info(JSON.stringify(process.env.VITE_APP_TITLE));
+    },
+};
+</script>
+<style lang="scss">
+// @import url(./assets/style/index.sass);
+</style>
